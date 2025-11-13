74553048269bb6595c8721dc67d6ebec0d37e4f9
bmarques1995 <testebash@outlook.com>
2025-11-13 18:44:21 -0300

    Adicionar arquivos de projeto.

---
+++ RepoFetcher/RepoFetcher.cpp
@@ -0,0 +1,12 @@
+﻿// RepoFetcher.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "RepoFetcher.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
